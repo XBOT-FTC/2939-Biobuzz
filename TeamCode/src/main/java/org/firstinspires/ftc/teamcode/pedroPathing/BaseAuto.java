86b7a1ccaf9657122fa409f1c04716df09a635fd
David Nguyen <infernaldestinymc@gmail.com>
2026-09-12 11:48:24 -0700

    Converted to ivy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BaseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BaseAuto.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.follower.Follower;
+import com.pedropathing.ivy.Command;
 import com.pedropathing.localization.Localizer;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import static com.pedropathing.ivy.groups.Groups.sequential;
+import static com.pedropathing.ivy.pedro.PedroCommands.follow;
+import com.pedropathing.ivy.Scheduler;
 
+import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.teamcode.Constants;
 
 
@@ -22,6 +27,9 @@ public class BaseAuto extends LinearOpMode {
         START,
         PARK,
         END
+    }
+    public BaseAuto() {
+        follower = Constants.createFollower(hardwareMap);
     }
 
     protected PathState pathState;
@@ -41,20 +49,24 @@ public class BaseAuto extends LinearOpMode {
         opModeTimer = new ElapsedTime();
 
         waitForStart();
-        opModeTimer.reset();
 
-        setPathState(PathState.START);
+        Scheduler.reset();
 
         if (isStopRequested()) return;
 
+        opModeTimer.reset();
+
+        Scheduler.schedule(autonomousStateMachine());
+
         while (opModeIsActive() && !isStopRequested()) {
             follower.update();
-
-            autonomousStateMachine();
         }
+
     }
 
-    protected void autonomousStateMachine() {
-        // Autos are to be placed here
+    protected Command autonomousStateMachine() {
+        return sequential(
+                // Autos are to be placed here
+        );
     }
 }
