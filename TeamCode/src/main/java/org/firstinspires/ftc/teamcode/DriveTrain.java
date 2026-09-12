a76c42c01f8513b0df490391c43aa4490dbfa78e
Victor C <vich2021@gmail.com>
2026-09-12 11:03:43 -0700

    feat: Create Drive Train Base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class DriveTrain {
+    private final DcMotor leftFront;
+    public double leftFrontPower;
+    public double getLeftFrontPower() {
+        return leftFrontPower;
+    }
+    private final DcMotor leftRear;
+    public double leftRearPower;
+    public double getLeftRearPower() {
+        return leftRearPower;
+    }
+    private final DcMotor rightFront;
+    public double rightFrontPower;
+    public double getRightFrontPower() {
+        return rightFrontPower;
+    }
+    private final DcMotor rightRear;
+    public double rightRearPower;
+    public double getRightRearPower() {
+        return rightRearPower;
+    }
+
+    public DriveTrain(HardwareMap hwMap, Telemetry telemetry) {
+        this.leftFront = hwMap.dcMotor.get(Constants.leftFront());
+        this.leftRear = hwMap.dcMotor.get(Constants.leftRear());
+        this.rightFront = hwMap.dcMotor.get(Constants.rightFront());
+        this.rightRear = hwMap.dcMotor.get(Constants.rightRear());
+    }
+
+    public void gamepadConfigurator(Gamepad gamepad, Telemetry telemetry) {
+        // Implementation for gamepad configuration
+        double y = gamepad.left_stick_y;
+        double x = gamepad.left_stick_x;
+        double rx = -gamepad.right_stick_x;
+
+
+        double inputNormalization = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        this.leftFrontPower = (y + x + rx) / inputNormalization;
+        this.leftRearPower = (y - x + rx) / inputNormalization;
+        this.rightFrontPower = (y - x - rx) / inputNormalization;
+        this.rightRearPower = (y + x - rx) / inputNormalization;
+    }
+}
